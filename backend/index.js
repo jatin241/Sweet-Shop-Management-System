37544ce689f47bb7ac36df024110b7a10fa29c35
jatin241 <jatin759936@gmail.com>
2025-09-24 19:55:26 +0530

    add middlewares for express.json and cors and reverting back to commonjs for testing on jest

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
-import express from "express";
-import mongoose from 'mongoose';
-import "dotenv/config"
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require("cors");
+require('dotenv').config()
 
 const app=express();
-//connection
+
+app.use(express.json())
+app.use(cors())
+
 async function dbConnect() {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/userRegistration')
